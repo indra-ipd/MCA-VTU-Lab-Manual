2012f3bb820af80b63421a3f865783475d2c939e
indra-ipd <indra.ipd@gmail.com>
2014-01-19 16:04:10 +0530

    Update Prog8.java

--- Sem3/Java_Prog_Lab/Prog8.java
+++ Sem3/Java_Prog_Lab/Prog8.java
@@ -1,10 +1,10 @@
-public enum DaysOfWeek
+public enum DayOfWeek
 { 
 	// Enumeration of days of week
 	
 	MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY;
 	// isWorkday method : returns false if Saturday or Sunday else returns false
-	public boolean isWorkday()
+	public boolean isWorkDay()
 	{
 		// switch case
 		switch (this)
@@ -24,12 +24,12 @@ class test
 {
      public static void main(String[] args)
      {
-	System.out.println("Sunday : " + DaysOfWeek.SUNDAY.isWorkday());
-	System.out.println("Monday : " + DaysOfWeek.MONDAY.isWorkday());
-	System.out.println("Tuesday : " + DaysOfWeek.TUESDAY.isWorkday());
-	System.out.println("Wednesday : " + DaysOfWeek.WEDNESDAY.isWorkday());
-	System.out.println("Thursday : " + DaysOfWeek.THURSDAY.isWorkday());
-	System.out.println("Friday : " + DaysOfWeek.FRIDAY.isWorkday());
-	System.out.println("Saturday : " + DaysOfWeek.SATURDAY.isWorkday());
+	System.out.println("Sunday : " + DayOfWeek.SUNDAY.isWorkDay());
+	System.out.println("Monday : " + DayOfWeek.MONDAY.isWorkDay());
+	System.out.println("Tuesday : " + DayOfWeek.TUESDAY.isWorkDay());
+	System.out.println("Wednesday : " + DayOfWeek.WEDNESDAY.isWorkDay());
+	System.out.println("Thursday : " + DayOfWeek.THURSDAY.isWorkDay());
+	System.out.println("Friday : " + DayOfWeek.FRIDAY.isWorkDay());
+	System.out.println("Saturday : " + DayOfWeek.SATURDAY.isWorkDay());
       }
 }
